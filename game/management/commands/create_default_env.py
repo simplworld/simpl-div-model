d7620d9c9a2a9fff6bc223cd353c6da0cab3ce54
Jane Eisenstein <ejane@wharton.upenn.edu>
2019-08-08 15:06:27 -0400

    Refactored to use sync games_client.

--- game/management/commands/create_default_env.py
+++ game/management/commands/create_default_env.py
@@ -1,7 +1,6 @@
 import djclick as click
 
-from modelservice.simpl import games_client
-from modelservice.utils.asyncio import coro
+from modelservice.simpl.sync import games_client
 
 
 def echo(text, value):
@@ -10,20 +9,19 @@ def echo(text, value):
     )
 
 
-async def delete_default_run(api_session):
+def delete_default_run(games_client):
     """ Delete default Run """
     echo('Resetting the Simpl Div game default run...', ' done')
-    runs = await api_session.runs.filter(game_slug='simpl-div')
+    runs = games_client.runs.filter(game_slug='simpl-div')
     for run in runs:
         if run.name == 'default':
-            await api_session.runs.delete(run.id)
+            games_client.runs.delete(run.id)
 
 
 @click.command()
 @click.option('--reset', default=False, is_flag=True,
               help="Delete default game run and recreate it from scratch")
-@coro
-async def command(reset):
+def command(reset):
     """
     Create and initialize Simpl Div game.
     Create a "default" Simpl Div run.
@@ -35,62 +33,60 @@ async def command(reset):
     splitting the players between the 2 worlds and assigning all roles.
     """
 
-    async with games_client as api_session:
+    # Create a Game
+    game = games_client.games.get_or_create(
+        name='Simpl Div',
+        slug='simpl-div'
+    )
+    echo('getting or creating game: ', game.name)
 
-        # Create a Game
-        game = await api_session.games.get_or_create(
-            name='Simpl Div',
-            slug='simpl-div'
-        )
-        echo('getting or creating game: ', game.name)
+    # Handle resetting the game
+    if reset:
+        if click.confirm(
+                'Are you sure you want to delete the default game run and recreate from scratch?'):
+            delete_default_run(games_client)
 
-        # Handle resetting the game
-        if reset:
-            if click.confirm(
-                    'Are you sure you want to delete the default game run and recreate from scratch?'):
-                await delete_default_run(api_session)
+    # Create required Roles ("Dividend" and "Divisor")
+    dividend_role = games_client.roles.get_or_create(
+        game=game.id,
+        name='Dividend',
+    )
+    echo('getting or creating role: ', dividend_role.name)
 
-        # Create required Roles ("Dividend" and "Divisor")
-        dividend_role = await api_session.roles.get_or_create(
-            game=game.id,
-            name='Dividend',
-        )
-        echo('getting or creating role: ', dividend_role.name)
+    divisor_role = games_client.roles.get_or_create(
+        game=game.id,
+        name='Divisor',
+    )
+    echo('getting or creating role: ', divisor_role.name)
 
-        divisor_role = await api_session.roles.get_or_create(
-            game=game.id,
-            name='Divisor',
-        )
-        echo('getting or creating role: ', divisor_role.name)
+    # Create game Phases ("Play" and "Debrief")
+    play_phase = games_client.phases.get_or_create(
+        game=game.id,
+        name='Play',
+        order=1,
+    )
+    echo('getting or creating phase: ', play_phase.name)
 
-        # Create game Phases ("Play" and "Debrief")
-        play_phase = await api_session.phases.get_or_create(
-            game=game.id,
-            name='Play',
-            order=1,
-        )
-        echo('getting or creating phase: ', play_phase.name)
+    debrief_phase = games_client.phases.get_or_create(
+        game=game.id,
+        name='Debrief',
+        order=2,
+    )
+    echo('getting or creating phase: ', debrief_phase.name)
 
-        debrief_phase = await api_session.phases.get_or_create(
-            game=game.id,
-            name='Debrief',
-            order=2,
-        )
-        echo('getting or creating phase: ', debrief_phase.name)
+    # Add run with 2 fully populated worlds ready to play
+    run = add_run(game, 'default', 2, 1,
+                        dividend_role, divisor_role,
+                        play_phase, games_client)
 
-        # Add run with 2 fully populated worlds ready to play
-        run = await add_run(game, 'default', 2, 1,
-                            dividend_role, divisor_role,
-                            play_phase, api_session)
-
-        # echo('Completed setting up run: id=', run.id)
+    # echo('Completed setting up run: id=', run.id)
 
 
-async def add_run(game, run_name, world_count, first_user_number,
+def add_run(game, run_name, world_count, first_user_number,
                   dividend_role, divisor_role,
-                  phase, api_session):
+                  phase, games_client):
     # Create or get a Run
-    run = await api_session.runs.get_or_create(
+    run = games_client.runs.get_or_create(
         game=game.id,
         name=run_name,
     )
@@ -98,7 +94,7 @@ async def add_run(game, run_name, world_count, first_user_number,
 
     # Set run to phase
     run.phase = phase.id
-    await run.save()
+    run.save()
     echo('setting run to phase: ', phase.name)
 
     user_name_root = "s"
@@ -106,33 +102,33 @@ async def add_run(game, run_name, world_count, first_user_number,
         user_name_root = run_name
     for n in range(0, world_count):
         world_num = n + 1
-        world = await add_world(run, world_num, api_session)
+        world = add_world(run, world_num, games_client)
 
         # Add users to run
-        await add_world_users(run, world, user_name_root,
+        add_world_users(run, world, user_name_root,
                               first_user_number + n * 2,
-                              dividend_role, divisor_role, api_session)
+                              dividend_role, divisor_role, games_client)
 
     return run
 
 
-async def add_world(run, number, api_session):
+def add_world(run, number, games_client):
     """
         Add a world to the run with a scenario and period 1.
         The world's name is based on number.
     """
     name = 'World {0}'.format(number)
-    world = await api_session.worlds.get_or_create(
+    world = games_client.worlds.get_or_create(
         run=run.id,
         name=name,
     )
     echo('getting or creating world: ', world.name)
 
-    scenario = await api_session.scenarios.create({
+    scenario = games_client.scenarios.create({
         'world': world.id,
         'name': 'World Scenario 1'
     })
-    period1 = await api_session.periods.create({
+    period1 = games_client.periods.create({
         'scenario': scenario.id,
         'order': 1
     })
@@ -140,15 +136,15 @@ async def add_world(run, number, api_session):
     return world
 
 
-async def add_world_users(run, world, user_name_root,
+def add_world_users(run, world, user_name_root,
                           first_number,
-                          dividend_role, divisor_role, api_session):
+                          dividend_role, divisor_role, games_client):
     """
         Add 1 leader ("leader") to the run with a test scenario
         Add players to the run with names based on user_name_root and first_number
         Add players to world assigning all required roles
     """
-    fac_user = await api_session.users.get_or_create(
+    fac_user = games_client.users.get_or_create(
         password='leader',
         first_name='Div',
         last_name='Leader',
@@ -156,7 +152,7 @@ async def add_world_users(run, world, user_name_root,
     )
     echo('getting or creating user: ', fac_user.email)
 
-    await api_session.runusers.get_or_create(
+    games_client.runusers.get_or_create(
         user=fac_user.id,
         run=run.id,
         leader=True,
@@ -166,12 +162,12 @@ async def add_world_users(run, world, user_name_root,
     roles = [dividend_role, divisor_role]
     for n in range(len(roles)):
         user_number = n + first_number
-        await add_player(user_name_root, user_number, run, world, roles[n],
-                         api_session)
+        add_player(user_name_root, user_number, run, world, roles[n],
+                         games_client)
 
 
-async def add_player(user_name_root, user_number, run, world, role,
-                     api_session):
+def add_player(user_name_root, user_number, run, world, role,
+                     games_client):
     """Add player with name based on user_name_root and user_number to world in role"""
 
     username = '{}{}'.format(user_name_root, user_number)
@@ -182,7 +178,7 @@ async def add_player(user_name_root, user_number, run, world, role,
         last_name = user_name_root[:1].upper() + user_name_root[1:]
     email = '{0}@div.edu'.format(username)
 
-    user = await api_session.users.get_or_create(
+    user = games_client.users.get_or_create(
         password=username,
         first_name=first_name,
         last_name=last_name,
@@ -190,7 +186,7 @@ async def add_player(user_name_root, user_number, run, world, role,
     )
     echo('getting or creating user: ', user.email)
 
-    await api_session.runusers.get_or_create(
+    games_client.runusers.get_or_create(
         user=user.id,
         run=run.id,
         world=world.id,
