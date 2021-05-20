84d75938e5ca587e548e4217d99c4d9cd381faf3
Jane Eisenstein <ejane@softweave.com>
2021-05-20 15:58:00 -0400

    Improves error handling.

--- game/profilers/profile_http.py
+++ game/profilers/profile_http.py
@@ -17,15 +17,15 @@ class ProfileHttpTestCase(ProfileCase):
             # print('profile_submit_decision: email=', email)
 
             # email format is <char><int>@ where <int> is 1..78
-            # delay = int(email[1:email.find('@')])
-            # await asyncio.sleep(delay * 0.5)
+            # which assumes run name is a single letter
+            decision = int(email[1:email.find('@')])
 
             coro_client = games_client_factory()
 
             async with coro_client as coro_session:
                 try:
                     # Determine Run and Runuser based on player email
-                    run_name = email[0]  # assumes run name is a single letter
+                    run_name = email[0] # run name is a single letter
                     run = await coro_session.runs.get(
                         game_slug=settings.GAME_SLUG,
                         name=run_name,
@@ -37,7 +37,8 @@ class ProfileHttpTestCase(ProfileCase):
                     )
 
                     # From here down, pull data from modelservice via WAMP
-                    # emulating the calls made by the simpl-react simpl decorator
+
+                    # First, emulate calls made by the simpl-react simpl decorator when a player logs in
 
                     world_topic = 'world.simpl.sims.simpl-div.model.world.' + str(runuser.world)
 
@@ -61,15 +62,6 @@ class ProfileHttpTestCase(ProfileCase):
 
                     runuser_topic = 'world.simpl.sims.simpl-div.model.runuser.' + str(runuser.id)
 
-                    #  getRunUsers(world_topic)
-                    await self.call(runuser_topic + '.get_active_runusers')  # ignore results
-
-                    # getCurrentRunPhase(world_topic)
-                    await self.call(runuser_topic + '.get_current_run_and_phase')  # ignore results
-
-                    # getDataTree(world_topic)
-                    await self.call(runuser_topic + '.get_scope_tree')  # ignore results
-
                     # getRunUserScenarios(runuser_topic)
                     get_scenarios_uri = runuser_topic + '.get_scenarios'
                     get_scenarios_result = await self.call(get_scenarios_uri)
@@ -88,23 +80,20 @@ class ProfileHttpTestCase(ProfileCase):
                     # print(get_roles_uri, '->')
                     # print(get_roles_result)
 
-                    for role in get_roles_result:
-                        name = role['name']
-                        id = role['id']
-                        if name == "Dividend":
-                            dividend_role = id
-                        elif name == "Divisor":
-                            divisor_role = id
-                        else:
-                            raise Exception("ERROR: unexpected role '" + name + "'")
+                    # Next, prepare to submit player's decision
 
-                    # debug simpl-div-ops#37
+                    # Check whether run is in Play phase
                     run_phase_name = get_current_run_and_phase_result['phase']['data']['name']
                     if run_phase_name != 'Play':
                         raise Exception("ERROR: Run must be in Play phase")
 
+                    # Check whether this world already has a result
+                    first_period = get_scope_tree_result['children'][0]['children'][0]
+                    if len(first_period['children']) == 3:
+                        raise Exception("ERROR: Player's world already has a result")
+
                     # get id of first period of first scenario of player's world
-                    first_period_id = get_scope_tree_result['children'][0]['children'][0]['pk']
+                    first_period_id = first_period['pk']
                     # print('first_period_id: ', first_period_id)
 
                 except Exception as e:
@@ -114,12 +103,6 @@ class ProfileHttpTestCase(ProfileCase):
                 # submit player's decision
                 uri = 'world.simpl.sims.simpl-div.model.period.' + \
                       str(first_period_id) + '.submit_decision'
-                if runuser.role is dividend_role:
-                    decision = 5
-                elif runuser.role is divisor_role:
-                    decision = 2
-                else:
-                    decision = None
 
                 if decision is not None:
                     status = await self.call_as(email, uri, decision)
